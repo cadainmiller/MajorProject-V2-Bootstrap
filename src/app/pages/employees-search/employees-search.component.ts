fc094d3566245c8698cb145eed14f9c6ce6d5e3c
Cadain <cmiller@aventmark.com>
2020-08-05 16:27:03 -0500

    Working on ag-grid

--- src/app/pages/employees-search/employees-search.component.ts
+++ src/app/pages/employees-search/employees-search.component.ts
@@ -3,6 +3,7 @@ import { ColDef, GridApi, ColumnApi } from 'ag-grid-community';
 import { EmployeesService } from 'src/app/services/employees.service';
 import { HttpClient } from '@angular/common/http';
 
+
 var $: any;
 
 @Component({
@@ -10,22 +11,56 @@ var $: any;
   templateUrl: './employees-search.component.html',
   styleUrls: ['./employees-search.component.scss'],
 })
+
+
 export class EmployeesSearchComponent implements OnInit {
-
   columnDefs = [
-    { headerName: 'Make', field: 'make', sortable: true, filter: true },
-    { headerName: 'Model', field: 'model', sortable: true, filter: true },
-    { headerName: 'Price', field: 'price', sortable: true, filter: true },
-  ];
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { headerName: 'Name', field: 'name', sortable: true, filter: true },
+    {
+      headerName: 'Username',
+      field: 'username',
+      sortable: true,
+      filter: true,
+      editable: true,
+      suppressSorting: true,
+    },
+    {
+      headerName: 'Phone Number',
+      field: 'phone',
+      sortable: true,
+      filter: true,
+      
+    },
+    {
+      headerName: 'Street',
+      field: 'address.street',
+      sortable: true,
+      filter: true,
+    },
+    { headerName: 'City', field: 'address.city', sortable: true, filter: true },
+    {
+      headerName: 'Zip',
+      field: 'address.zipcode',
+      sortable: true,
+      filter: true,
+      
+    },
   ];
 
-  constructor(private http: HttpClient) {}
+  
+  rowData = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private http: HttpClient,
+    private employeeService: EmployeesService
+  ) {}
+
+  ngOnInit() {
+    this.employeeService.sendGetRequest().subscribe((results) => {
+      this.rowData = results;
+      console.log(results);
+    });
+  }
 
   onRowClicked(event: any) {
     $('#imagemodal').modal('show');
